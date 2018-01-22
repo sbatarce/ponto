d2fc786fc2932a16ea3bb699583e9ae2843846e0
W0513263 <sergio-batarce@prodesan.com.br>
2018-01-22 14:30:13 -0200

    vlfuncs

--- services/vlfuncs.php
+++ services/vlfuncs.php
@@ -3,6 +3,19 @@ include '../partes/fmtErro.php';
 include '../partes/ambiente.php';
 include '../partes/ORAConn.php';
 
+//	obtem authentication
+if( isset( $_SERVER['PHP_AUTH_USER'] ) )
+	{
+	$userb = $_SERVER['PHP_AUTH_USER'];
+	$passb = $_SERVER['PHP_AUTH_PW'];
+	}
+else
+	{
+	echo "{ \"status\": \"erro\", \"erro\": \"Acesso negado. Falta Authentication-basic\" }";
+	return;
+	}
+
+//	verifica o method
 $meth = $_SERVER['REQUEST_METHOD'];
 if( $meth != 'POST' )
 	{
@@ -30,21 +43,10 @@ if( $json == null )
 	fmtErro( "erro", "falta o JSON no corpo da mensagem" );
 	return;
 	}
-//	verifica os dados do JSON
-if( !isset($json->sshd) )
-	{
-	fmtErro( "erro", "sshd&senha obrigatorios no corpo da mensagem" );
-	return;
-	}
-if( !isset($json->pass) )
-	{
-	fmtErro( "erro", "sshd&senha obrigatorios no corpo da mensagem" );
-	return;
-	}
-$user = $json->sshd;
-$pass = $json->pass;
+//	obtem e verifica o usuario e senha do funcionario
+$sshd = $json->sshd;
 //	verifica o SSHD
-$conn = @oci_connect( $user, $pass, $amb, $chset, OCI_DEFAULT );
+$conn = @oci_connect( $userb, $passb, $amb, $chset, OCI_DEFAULT );
 if( !$conn )
   {
   $e = oci_error();
@@ -58,7 +60,7 @@ $sql	=	"SELECT FUNI.FUNI_ID, FUAU.PMS_IDSAUUOR
 					LEFT JOIN BIOMETRIA.FUAU_FUNCIONARIOAUTORIZADOR FUAU ON
 										FUAU.FUNI_ID=FUNI.FUNI_ID AND
 										FUAU.FUAU_DTFIM IS NULL
-					WHERE FUNI.PMS_IDPMSPESSOA='$user'";
+					WHERE FUNI.PMS_IDPMSPESSOA='$sshd'";
 $stid = oci_parse( $conn, $sql );
 if( !$stid )
   {
@@ -79,7 +81,7 @@ if( !$r )
 $row = oci_fetch_array( $stid, OCI_ASSOC + OCI_RETURN_NULLS );
 if( !$row )
 	{
-  echo "{ \"status\": \"erro\", \"erro\":\"usuario não localizado\" }";
+  echo "{ \"status\": \"erro\", \"erro\":\"funcionário não localizado no sistema de ponto\" }";
 	return;
 	}
 $funiid = $row['FUNI_ID'];
